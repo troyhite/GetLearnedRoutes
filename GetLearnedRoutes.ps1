d1a1bcd4302328afaf6afd0864625850a6c9edbb
Troy Hite <troyhite@microsoft.com>
2022-09-19 17:23:27 -0500

    Powershell code for Azure Function

---
+++ GetLearnedRoutes.ps1
@@ -0,0 +1,22 @@
+# Input bindings are passed in via param block.
+param($Timer)
+#Subscription
+$subscriptionId = '<subid>'
+#Resource Group
+$resourceGroup = '<resource-group>'
+#VPN Gateway associated with vWAN S2S
+$vpngw = '<vpn-gateway>'
+# Set target Azure Subscription
+Set-AzContext -Subscription $subscriptionId
+# Set AzRest URI
+$restUri = "/subscriptions/$subscriptionId/resourceGroups/$resourceGroup/providers/Microsoft.Network/vpnGateways/$vpngw/getlearnedroutes?api-version=2022-01-01"
+# Call API and store respone
+$response = Invoke-AzRestMethod -Path $restUri -Method POST
+# Extract Location Header
+$location = $response.Headers.Location.PathAndQuery
+# GET on Location Header Key
+$results = Invoke-AzRestMethod -Path $location -Method GET
+# Convert Results to JSON
+$value = $($results.Content | ConvertFrom-Json ).Value | ConvertTo-Json
+# Push JSON to Storage Account
+Push-OutputBinding -Name jsonOutput -Value $value
